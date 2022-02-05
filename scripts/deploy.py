44cbd848f4f185cc5cc44f346c6e204cac90bee3
KR <kyranrawlinson@gmail.com>
2022-02-05 15:21:05 +0000

    ADDED brownie-config.yaml and UPDATED deploy.py to use config

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,7 +1,12 @@
-from brownie import accounts
+from brownie import accounts, config
 
 def deploy_simpleStorage():
-    account = accounts[0]
+    #local rpc account
+    #account = accounts[0]
+    
+    #live account
+    account = accounts.add(config['wallets']['dev_account_1']['private_key'])
+    print(account)
 
 def main():
     deploy_simpleStorage()
